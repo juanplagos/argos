a16d71db5eb4a25c70918e0de6a054d8a37b413d
juanplagos <juanlagospg@gmail.com>
2025-10-25 16:52:52 -0300

    refactor: rename variable 'result' to 'Connection' in TestPing function

--- internal/ping/ping_test.go
+++ internal/ping/ping_test.go
@@ -7,20 +7,20 @@ import (
 func TestPing(t *testing.T) {
 	t.Run("when host is reachable", func(t *testing.T) {
 
-		result := CheckHost("google.com")
+		Connection := CheckHost("google.com")
 		expect := "up"
 
-		if result.Status != expect {
-			t.Errorf("got %q, expected %q", result.Status, expect)
+		if Connection.Status != expect {
+			t.Errorf("got %q, expected %q", Connection.Status, expect)
 		}
 	})
 	t.Run("when host is unreachable", func(t *testing.T) {
 
-		result := CheckHost("invalid.host")
+		Connection := CheckHost("invalid.host")
 		expect := "down"
 
-		if result.Status != expect {
-			t.Errorf("got %q, expected %q", result.Status, expect)
+		if Connection.Status != expect {
+			t.Errorf("got %q, expected %q", Connection.Status, expect)
 		}
 	})
 }
