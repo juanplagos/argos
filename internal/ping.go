5622a7645c44569c81ccbee1ef69b6cad4eb85c7
juanplagos <juanlagospg@gmail.com>
2025-10-25 00:48:38 -0300

    feat: add CheckHost function to ping a host and return its status

--- internal/ping.go
+++ internal/ping.go
@@ -1,2 +1,24 @@
-package internal
+package ping
 
+import (
+	"strings"
+	"os/exec"
+)
+
+type result struct {
+	Status string
+	Latency string
+}
+
+func CheckHost(host string) result {
+	output, err := exec.Command("ping", "-c", "1", host).CombinedOutput()
+	parsed_out := string(output)
+
+	if err != nil || !strings.Contains(parsed_out, "1 received") {
+		//fmt.Println(parsed_out)
+		return result{Status: "down"}
+	} else {
+	//fmt.Println(parsed_out)
+	return result{Status: "up"}
+	}
+}
