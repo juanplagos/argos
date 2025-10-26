be872685784a1f31377c776f0b0d1650c02f429d
juanplagos <juanlagospg@gmail.com>
2025-10-26 16:36:41 -0300

    feat: enhance CheckHost function to return latency information

--- internal/ping/ping.go
+++ internal/ping/ping.go
@@ -2,6 +2,7 @@ package ping
 
 import (
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
@@ -14,9 +15,16 @@ func CheckHost(host string) Connection {
 	output, err := exec.Command("ping", "-c", "1", host).CombinedOutput()
 	parsed_out := string(output)
 
+	var latency string
+
+	latencyPattern := regexp.MustCompile(`time=([0-9]*\.?[0-9]+\s?ms)`)
+	if m := latencyPattern.FindStringSubmatch(parsed_out); len(m) > 1 {
+		latency = m[1]
+	}
+
 	if err != nil || !strings.Contains(parsed_out, "1 received") {
-	return Connection{Status: "down"}
+		return Connection{Status: "down", Latency: ""}
 	} else {
-	return Connection{Status: "up"}
+	return Connection{Status: "up", Latency: latency}
 	}
 }
